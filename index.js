4734054996e9e448091b0e3f52a9b5cf367aab92
Stéphane Francel <contact@avine.io>
2018-04-17 19:25:25 +0200

    Pretty title

--- index.js
+++ index.js
@@ -11,21 +11,23 @@ new TreeView({
 }).process(path.resolve(__dirname, './pages')).then(tree => {
   const flat = flatten(tree);
 
+  // Prepare
   const templateIndex = flat.findIndex(item => item.name === '_template.html');
   const template = flat.splice(templateIndex, 1)[0];
   const comment = `<!-- AUTO GENERATED FILE | ${new Date().toUTCString()} -->\n`;
-
-  function processTemplate(item) {
+  function processTemplate(_path, name, content) {
     const full = comment + template.content
-      .replace('{{title}}', item.name)
-      .replace('{{content}}', item.content);
+      .replace('{{title}}', fileNameToTitle(name))
+      .replace('{{content}}', content);
 
-    fs.writeFileSync(path.resolve(__dirname, './src', item.name), full, 'utf8');
-    console.log('Updated: ' + item.pathname);
+    fs.writeFileSync(path.resolve(__dirname, './src', name), full, 'utf8');
+    console.log('Updated:', path.resolve(_path, name));
   }
 
-  flat.forEach(item => processTemplate(item));
+  // Process
+  flat.forEach(item => processTemplate(item.path, item.name, item.content));
 
+  // Update
   const timeout = {};
   fs.watch(path.resolve(__dirname, './pages'), { recursive: true }, (eventType, filename) => {
     if (timeout[filename]) clearTimeout(timeout[filename]);
@@ -36,10 +38,15 @@ new TreeView({
       if (modified) {
         const content = fs.readFileSync(path.resolve(__dirname, './pages', modified.name), 'utf8');
         modified.content = content;
-        processTemplate(modified);
+        processTemplate(modified.path, modified.name, modified.content);
       } else {
         // TODO...
       }
     }, 100);
   });
 });
+
+function fileNameToTitle(filename) {
+  const title = filename.replace('.html', '').replace(/\-/g, ' ');
+  return title.substr(0, 1).toUpperCase() + title.substr(1);
+}

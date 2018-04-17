a09fe032eeb0f428f408f126be2c17c348671e98
Stéphane Francel <contact@avine.io>
2018-04-17 20:01:24 +0200

    Handle sub folder (in progress...)

--- index.js
+++ index.js
@@ -4,49 +4,62 @@ const fs = require('fs');
 const { TreeView } = require('node-treeview');
 const { flatten } = require('node-treeview/helper');
 
-// FIXME: it works only if './pages' has no sub folders.
+const TMPL_NAME = '_template.html';
+
+const PAGES_PATH = path.resolve(__dirname, './pages');
+const TARGET_PATH = path.resolve(__dirname, './src');
 
 new TreeView({
-  content: true
-}).process(path.resolve(__dirname, './pages')).then(tree => {
+  content: true,
+  relative: true
+}).process(PAGES_PATH).then(tree => {
   const flat = flatten(tree);
 
   // Prepare
-  const templateIndex = flat.findIndex(item => item.name === '_template.html');
-  const template = flat.splice(templateIndex, 1)[0];
+  const tmplIndex = flat.findIndex(item => item.name === TMPL_NAME && item.path === '');
+  const tmplItem = flat.splice(tmplIndex, 1)[0];
   const comment = `<!-- AUTO GENERATED FILE | ${new Date().toUTCString()} -->\n`;
-  function processTemplate(_path, name, content) {
-    const full = comment + template.content
-      .replace('{{title}}', fileNameToTitle(name))
+
+  function processTmpl(dirpath, filename, content) {
+    const html = comment + tmplItem.content
+      .replace('{{title}}', filenameToTitle(filename))
       .replace('{{content}}', content);
 
-    fs.writeFileSync(path.resolve(__dirname, './src', name), full, 'utf8');
-    console.log('Updated:', path.resolve(_path, name));
+    let fullname = dirpath.replace(/\/|\\/g, '-');
+    fullname = (fullname ? fullname + '-' : '') + filename;
+
+    fs.writeFileSync(path.resolve(TARGET_PATH, fullname), html, 'utf8');
+    console.log('Updated:', path.resolve(dirpath, filename));
   }
 
   // Process
-  flat.forEach(item => processTemplate(item.path, item.name, item.content));
+  flat.forEach(item => processTmpl(item.path, item.name, item.content));
 
   // Update
   const timeout = {};
-  fs.watch(path.resolve(__dirname, './pages'), { recursive: true }, (eventType, filename) => {
+  fs.watch(PAGES_PATH, { recursive: true }, (eventType, filename) => {
     if (timeout[filename]) clearTimeout(timeout[filename]);
     timeout[filename] = setTimeout(() => {
       delete timeout[filename];
 
       const modified = flat.filter(item => item.name === filename)[0];
       if (modified) {
-        const content = fs.readFileSync(path.resolve(__dirname, './pages', modified.name), 'utf8');
+        const content = fs.readFileSync(path.resolve(PAGES_PATH, modified.name), 'utf8');
         modified.content = content;
-        processTemplate(modified.path, modified.name, modified.content);
+        processTmpl(modified.path, modified.name, modified.content);
       } else {
-        // TODO...
+        // FIXME: sub folder still come here...!
+        console.log('Unable to process:', filename);
       }
     }, 100);
   });
 });
 
-function fileNameToTitle(filename) {
+function filenameToTitle(filename) {
   const title = filename.replace('.html', '').replace(/\-/g, ' ');
   return title.substr(0, 1).toUpperCase() + title.substr(1);
+}
+
+function log(data) {
+  console.log(JSON.stringify(data, undefined, 2));
 }

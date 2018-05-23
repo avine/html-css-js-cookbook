339c22d2b191178b056bed5ca6f88c5a57fce676
Stéphane Francel <contact@avine.io>
2018-05-23 23:55:52 +0200

    Make it work (webpack)

--- src/app.js
+++ src/app.js
@@ -1,2 +1,8 @@
-// import './front/scss/app.scss';
+import '../node_modules/prismjs/themes/prism.css';
+import '../node_modules/prismjs/prism';
+
+import './front/scss/settings.scss';
+import './front/scss/main.scss';
+import './front/scss/app.scss';
+
 import './front/js/app';
--- webpack.config.js
+++ webpack.config.js
@@ -2,27 +2,73 @@
 const path = require('path');
 
 const CleanPlugin = require('clean-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlPlugin = require('html-webpack-plugin');
 
+const devMode = false; // process.env.NODE_ENV !== 'production';
+
 module.exports = {
-  entry: './src/app.js',
+  mode: devMode ? 'development' : 'production',
+
+  target: 'web',
+
+  entry: {
+    app: './src/app.js',
+  },
 
   output: {
-    path: path.resolve(__dirname, 'dist/front'),
-    filename: 'app.bundle.[hash].js',
+    path: path.resolve(__dirname, './dist/front'),
+    filename: '[name].[hash].js',
   },
 
   module: {
     rules: [
+      {
+        test: /\.css$/,
+        use: [
+          devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+          'css-loader',
+        ],
+      },
+
+      {
+        test: /\.scss$/,
+        use: [
+          devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader',
+        ],
+      },
     ],
   },
 
   plugins: [
     new CleanPlugin('./dist'),
+
+    new CopyPlugin([
+      { from: './src/front/pages', to: './pages' },
+    ]),
+
+    new MiniCssExtractPlugin({
+      filename: devMode ? '[name].css' : '[name].[hash].css',
+      chunkFilename: devMode ? '[id].css' : '[id].[hash].css',
+    }),
+
     new HtmlPlugin({ template: './src/front/index.html' }),
   ],
 
   devtool: 'source-map',
 
-  mode: 'development',
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        commons: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          chunks: 'all',
+        },
+      },
+    },
+  },
 };

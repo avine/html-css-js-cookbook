bac0d314b8857b3493aac307cff06ea9a3c90363
Stéphane Francel <contact@avine.io>
2018-06-01 14:22:40 +0200

    Refactoring playground

--- src/front/ts/_playground.ts
+++ src/front/ts/_playground.ts
@@ -3,34 +3,60 @@ import { highlight, languages } from 'prismjs';
 import { createNode, insertAfter, querySelectorAll } from './_dom';
 import { ON_NAVIGATE } from './_router';
 
-function playHtml(playground: Element) {
+function playJs(playground: HTMLScriptElement, action: Element) {
+  // TODO...
+}
+
+function playCss(playground: HTMLStyleElement, action: Element) {
+  action.addEventListener('click', (event) => {
+    playground.disabled = !playground.disabled;
+  });
+}
+
+function playHtml(playground: Element, action: Element) {
   playground.classList.add('app-playground');
   playground.classList.add('app-playground--demo');
 }
 
-function insertSource(playground: Element, type: SourceType, anchor?: Element | null) {
-  const source = playground.innerHTML.trim().replace(/\n{2,}/g, '\n\n');
-  const code = createNode('<pre class="app-playground"><code class="app-playground__code"></code></pre>');
-  (code.firstChild as Element).innerHTML = highlight(source, languages[type], languages[type]);
-  code.appendChild(createNode(`<div class="app-playground__label">${type}</div>`));
-  insertAfter(code, anchor || playground);
+function formatSource(html: string) {
+  return html.trim().replace(/\n{2,}/g, '\n\n');
+}
+
+function getLabel(type: SourceType) {
+  return createNode(`<span class="app-playground__action app-playground__action--disabled">${type}</span>`);
+}
+
+function getAction(type: SourceType, wrap: Element) {
+  const action = createNode(`<a href="#" class="app-playground__action">${type}</a>`);
+  action.addEventListener('click', (event) => {
+    event.preventDefault();
+    wrap.classList.toggle('app-playground--disabled');
+  });
+  return action;
+}
+
+function insertSource(playground: Element, type: SourceType) {
+  const wrap = createNode('<pre class="app-playground"></pre>');
+  const code = createNode('<code class="app-playground__code"></code>');
+  const action = type === 'css' ? getAction(type, wrap) : getLabel(type);
+  const source = formatSource(playground.innerHTML);
+  code.innerHTML = highlight(source, languages[type], languages[type]);
+  wrap.appendChild(code);
+  wrap.appendChild(action);
+  const anchor = document.querySelector(`[app-playground-anchor-${type}]`);
+  insertAfter(wrap, anchor || playground);
+  return { wrap, code, action };
 }
 
 function enablePlayground(playground: Element) {
-  let type: SourceType;
-  switch (playground.nodeName.toLowerCase()) {
-    case 'script':
-      type = 'js';
-      break;
-    case 'style':
-      type = 'css';
-      break;
-    default:
-      type = 'html';
-      playHtml(playground);
-      break;
+  const nodeName = playground.nodeName.toLowerCase();
+  const type: SourceType = nodeName === 'script' ? 'js' : nodeName === 'style' ? 'css' : 'html';
+  const source = insertSource(playground, type);
+  switch (type) {
+    case 'js': playJs(playground as HTMLScriptElement, source.action); break;
+    case 'css': playCss(playground as HTMLStyleElement, source.action); break;
+    case 'html': playHtml(playground as Element, source.action); break;
   }
-  insertSource(playground, type, document.querySelector(`[app-playground-${type}]`));
 }
 
 function playgroundHandler() {

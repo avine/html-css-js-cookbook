07d4261d10a3a2f75169e30305893918d7ae8547
Stéphane Francel <contact@avine.io>
2018-06-10 19:13:53 +0200

    Handle noInternetAccess

--- src/front/ts/_fetch.ts
+++ src/front/ts/_fetch.ts
@@ -1,6 +1,15 @@
 import { FRONT_PAGES_FOLDER } from '../../config';
 import { resolveUrl } from './_dom';
 
+const noInternetAccess =
+`<p class="app-content__error">
+  <br>
+  <i class="fas fa-3x fa-exclamation"></i>
+  <br>
+  <br>
+  No internet access
+</p>`;
+
 export function fetchContent404() {
   return fetch(resolveUrl(`/${FRONT_PAGES_FOLDER}/error404.html`)).then(response => response.text());
 }
@@ -12,5 +21,6 @@ export function fetchContent(url: string) {
         return fetchContent404().then(html => html.replace('{{url}}', url));
       }
       return response.text();
-    });
+    })
+    .catch(() => noInternetAccess);
 }
